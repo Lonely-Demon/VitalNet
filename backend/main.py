b6edfc9eefbd902e4ca36df94161b340bc67c3f8
Lonely-Demon <lonelydemon61@gmail.com>
2026-03-12 21:08:47 +0530

    Fix CORS trailing slash mismatch

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         'http://localhost:5173',
-        os.getenv('FRONTEND_URL', ''),
+        'http://127.0.0.1:5173',
+        os.getenv('FRONTEND_URL', '').rstrip('/'),
     ],
     allow_credentials=True,
     allow_methods=['*'],
